000f8de1375115fe98398a6088907b81bc9f4e9d
Mariano Sciuto <marianosciuto.ok@gmail.com>
2025-08-13 15:48:28 -0300

    Modificando UML

--- src/main/java/ar/com/codigomariano/domain/PreguntaBinaria.java
+++ src/main/java/ar/com/codigomariano/domain/PreguntaBinaria.java
@@ -3,14 +3,13 @@ package ar.com.codigomariano.domain;
 import ar.com.codigomariano.enums.Categoria;
 import ar.com.codigomariano.enums.Opcion;
 
-public class PreguntaBinaria extends Pregunta {
-	private final int CANT_OPCIONES = 2; 
+public class PreguntaBinaria extends Pregunta { 
 	private Boolean[] opciones;
 	
 	
 	public PreguntaBinaria(String cod, String text, Categoria categoria, Opcion correcta, int puntos) {
 		super(cod, text, categoria, correcta, puntos);
-		this.opciones = new Boolean[CANT_OPCIONES];
+		this.opciones = new Boolean[] {Boolean.TRUE, Boolean.FALSE};
 	}
 
 }
